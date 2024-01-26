f7504b07e31bcb3609874efb910876e624435cfd
rigo mac <mbandrigo@gmail.com>
2024-01-26 15:16:51 +0800

    upload1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if (env('APP_ENV') == 'production')
+            URL::forceScheme('https');
     }
 }
